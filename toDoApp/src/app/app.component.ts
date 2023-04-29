31ec3a8b61a1a9a90794ba01c49f997d4b3b36b8
sehersaygili <77735385+sehersaygili@users.noreply.github.com>
2023-04-29 13:06:21 +0300

    add bootstrap to the project

--- toDoApp/src/app/app.component.ts
+++ toDoApp/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //selector: 'app-root',
+  selector: 'app',
+  templateUrl: './app.component.html', 
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDoApp';
-}
+
+  toDoItem = {
+    description: "kahvaltı",
+    action: true
+  }
+
+} 
